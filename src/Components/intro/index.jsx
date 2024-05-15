f5291da23422c392e0f40f1ef362bcccbcbd1e9e
Udeogu Alex <alexanderudeogu@gmail.com>
2024-05-15 13:48:48 +0100

    Testing for vercel

--- src/Components/intro/index.jsx
+++ src/Components/intro/index.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
   return (
     <section id="intro">
         <div className="introContent">
-            <span className="introText">ALEX </span> <span className='introText'>UDEOGU</span>
+            <span className="introText">ALEXX </span> <span className='introText'>UDEOGU</span>
             <p className="introPara">
             Aha na aha nna Alex Udeogu. nke "Udeogu" nke pụtara ikpe ziri ezi.<br /> Nabata na peeji nke m.
             </p>
